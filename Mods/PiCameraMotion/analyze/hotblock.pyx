24947cd8c0f6ec23393aff0b57a0d3c9231d32c6
Marcel Hetzendorfer <marcel@erazer.lan>
2019-03-29 21:43:15 +0100

    exception message please?

--- Mods/PiCameraMotion/analyze/hotblock.pyx
+++ Mods/PiCameraMotion/analyze/hotblock.pyx
@@ -11,7 +11,7 @@ ctypedef sBLOCK BLOCK
 
 @cython.boundscheck(False) # turn off bounds-checking for entire function
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
-cdef BLOCK chotBlock(np.ndarray[BLOCK, ndim=2] arr, np.int16_t rows, np.int16_t cols):
+def chotBlock(np.ndarray[BLOCK, ndim=2] arr, np.int16_t rows, np.int16_t cols):
     cdef BLOCK hotest
     hotest.x = 0
     hotest.y = 0
@@ -28,10 +28,6 @@ def hotBlock(a):
     print("Rows: Typ: {}, länge: {}".format(a.__class__.__name__, rows))
     cols = len(a[0])
     print("Cols: Typ: {}, länge: {}".format(a[0].__class__.__name__, cols))
-    try:
-        meh = chotBlock(a, rows, cols)            # here you can put your code
-    except Exception as e:
-        print(e)                # this prints error message   
-        print(type(e).__name__)
+    meh = chotBlock(a, rows, cols)
     print(meh.__class__.__name__)
     return meh.x, meh.y, meh.SAD
