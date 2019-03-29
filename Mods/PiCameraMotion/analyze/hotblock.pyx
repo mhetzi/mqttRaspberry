c8b6cf9719bb98c235e9958daf153e582e182708
Marcel Hetzendorfer <marcel@erazer.lan>
2019-03-29 21:37:45 +0100

    Mehr debug prints

--- Mods/PiCameraMotion/analyze/hotblock.pyx
+++ Mods/PiCameraMotion/analyze/hotblock.pyx
@@ -2,15 +2,20 @@ import numpy as np
 cimport numpy as np
 cimport cython
 
-cdef struct BLOCK:
+cdef struct sBLOCK:
     np.int8_t x
     np.int8_t y
     np.uint16_t SAD
+
+ctypedef sBLOCK BLOCK
 
 @cython.boundscheck(False) # turn off bounds-checking for entire function
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
 cdef BLOCK chotBlock(np.ndarray[BLOCK, ndim=2] arr, np.int16_t rows, np.int16_t cols):
     cdef BLOCK hotest
+    hotest.x = 0
+    hotest.y = 0
+    hotest.SAD = 0
     for r in range(rows):
         for c in range(cols):
             b = arr[r][c]
@@ -19,9 +24,10 @@ cdef BLOCK chotBlock(np.ndarray[BLOCK, ndim=2] arr, np.int16_t rows, np.int16_t 
     return hotest
 
 def hotBlock(a):
-    print(a.__class__.__name__)
     rows = len(a)
+    print("Rows: Typ: {}, länge: {}".format(a.__class__.__name__, rows))
     cols = len(a[0])
+    print("Cols: Typ: {}, länge: {}".format(a[0].__class__.__name__, cols))
     meh = chotBlock(a, rows, cols)
     print(meh.__class__.__name__)
     return meh.x, meh.y, meh.SAD
