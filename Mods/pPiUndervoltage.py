241542e4ff09e8fdca20f108a6cabfa24c48b70a
Marcel Hetzendorfer <mh7596@gmail.com>
2021-01-25 14:57:31 +0100

    Raspberry Undervoltage Plugin

---
+++ Mods/pPiUndervoltage.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as mclient
+import Tools.Config as conf
+import Tools.Autodiscovery as ad
+import logging
+import os
+import re
+import schedule
+import weakref
+
+try:
+    from rpi_bad_power import new_under_voltage
+except ImportError as ie:
+    try:
+        import Tools.error as err
+        err.try_install_package('rpi-bad-power', throw=ie, ask=True)
+    except err.RestartError:
+        from rpi_bad_power import new_under_voltage
+
+from Tools.Devices import BinarySensor
+
+class PluginLoader:
+
+    @staticmethod
+    def getConfigKey():
+        return "rPiUndervoltage"
+
+    @staticmethod
+    def getPlugin(client: mclient.Client, opts: conf.BasicConfig, logger: logging.Logger, device_id: str):
+        return RaspberryPiUndervoltageDetector(client, opts, logger, device_id)
+
+    @staticmethod
+    def runConfig(conf: conf.BasicConfig, logger:logging.Logger):
+        RaspberryPiUndervoltageConfig(conf).run()
+
+
+class RaspberryPiUndervoltageDetector:
+    _topic = None
+    _shed_Job = None
+    _plugin_manager = None
+
+    def __init__(self, client: mclient.Client, opts: conf.BasicConfig, logger: logging.Logger, device_id: str):
+        self._config = conf.PluginConfig(opts, PluginLoader.getConfigKey())
+        self.__client = client
+        self.__logger = logger.getChild(PluginLoader.getConfigKey())
+        self._prev_deg = None
+        self.__lastTemp = 0.0
+        self.__ava_topic = device_id
+        self._callables = []
+        self.device = None
+
+    def add_undervoltage_call(self, call):
+        if callable(call):
+            self._callables.append(call)
+
+    def set_pluginManager(self, pm):
+        self._plugin_manager = weakref.ref(pm)
+
+    def register(self):
+        t = ad.Topics.get_std_devInf()
+        self.device = BinarySensor.BinarySensor(
+            self.__logger,
+            self._plugin_manager(),
+            self._config.get("name", "Undervoltage"),
+            BinarySensor.autodisc.BinarySensorDeviceClasses.PROBLEM,
+            ""
+            )
+        self.device.register()
+        self._shed_Job = schedule.every( self._config.get("checks", 1) ).seconds
+        self._shed_Job.do(self.send_update)
+
+    def stop(self):
+        schedule.cancel_job(self._shed_Job)
+
+    def sendStates(self):
+        self.send_update(True)
+
+    def send_update(self, force=False):
+        undervoltage = new_under_voltage()
+
+        if undervoltage is None:
+            self.__logger.error("Undervoltage auf diesem System nicht unterstützt!")
+        elif undervoltage.get() != self._prev_deg and not force:
+            return
+
+        for call in self._callables:
+            try:
+                call(undervoltage)
+            except:
+                self.__logger.exception("Undervoltage callback error")
+
+        if undervoltage is not  None and self._prev_deg is not None:
+            self.__client.publish(self._topic.ava_topic, "online", retain=True)
+            self.__client.publish(self._topic.state, self.device.turn(undervoltage.get()))
+        elif undervoltage is not None:
+            self.__client.publish(self._topic.state, self.device.turn(undervoltage.get()))
+        else:
+            self.__client.publish(self._topic.ava_topic, "offline", retain=True)
+        self._prev_deg = None if undervoltage is None else undervoltage.get()
+
+
+class RaspberryPiUndervoltageConfig:
+    def __init__(self, conf: conf.BasicConfig):
+        self.c = conf
+
+    def run(self):
+        from Tools import ConsoleInputTools as cit
+        self.c["rPiUndervoltage/name"] = cit.get_input("Unter welchem Namen soll die Strom unterfversorgung angegeben werden. \n-> ", require_val=True, std_val="Undervoltage")
+        self.c["rPiUndervoltage/checks"] = cit.get_number_input("Wie oft soll getestet werden (Sekunen)?= ", 1)
